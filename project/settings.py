28cadad6c5910b68a13573d642b15eec43c0a70a
unknown <mohanraj.ns2020@gmail.com>
2025-07-01 12:36:01 +0530

    Set MySQL config using Railway environment variables

--- project/settings.py
+++ project/settings.py
@@ -115,14 +115,20 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'zone_db',
+        # 'USER':'root',
+        # 'PASSWORD':'',
+        # 'OPTIONS': {
+        #             'charset': 'utf8mb4',
+        #             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        #             }
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'zone_db',
-        'USER':'root',
-        'PASSWORD':'',
-        'OPTIONS': {
-                    'charset': 'utf8mb4',
-                    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-                    }
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     },
     
 }
