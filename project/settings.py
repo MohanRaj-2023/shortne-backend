be4ae38a4e011c9dfb87c4116fa2490366fab4ac
unknown <mohanraj.ns2020@gmail.com>
2025-07-05 16:32:28 +0530

    update settings for cloud config

--- project/settings.py
+++ project/settings.py
@@ -148,9 +148,9 @@ DATABASES = {
 # }
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('dq8biwq8q'),
-    'API_KEY': os.getenv('332996447824889'),
-    'API_SECRET': os.getenv('g2fpCYqqRTdcf6FDnbffF3WlNa4')
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET')
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
