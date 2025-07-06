44b39543f02f05c99cc916f01aa73fc44ac1a97b
unknown <mohanraj.ns2020@gmail.com>
2025-07-06 13:00:31 +0530

    update chache in settings for redis

--- project/settings.py
+++ project/settings.py
@@ -259,12 +259,22 @@ EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # chache
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",  # Make sure same DB is used
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient"
+#         }
+#     }
+# }
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",  # Make sure same DB is used
+        "LOCATION": os.getenv("REDIS_URL") + "/1",
         "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "SSL": True,
         }
     }
 }
